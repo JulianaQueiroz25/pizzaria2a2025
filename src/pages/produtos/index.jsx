cef3548f41436f500bedca1821b2fc41297cd70d
JulianaQueiroz25 <rm93965@estudante.fieb.edu.br>
2025-09-15 12:28:38 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Produtos = () => {
+
+    const [dados,setDados]= useState([]);
+
+    const pegarPizzas = async() => {
+
+        axios.get(" http://172.19.0.49/pizzariaoficial/api/v1/produto")
+       ( {withCredentials: true}
+    )
+        .then((response) => setDados(response.data))
+        .catch((error) => console.log(error))
+
+    }
+    
+    useEffect(()=>{
+        pegarPizzas();
+    }, [])
+    }
+
+    useEffect(()=>{
+        console.log(dados)
+    }, [dados])
+
+
 
     //Objeto de produtos da lista
     const pizzas =[
